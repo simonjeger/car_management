4aa10585e8d38ac7e05bbea2f86f73a82c9f0b55
simonjeger <simonjeger@gmail.com>
2020-01-20 12:31:55 +0100

    Switching Git Client

--- ORDER.py
+++ ORDER.py
@@ -1,10 +1,23 @@
 import numpy as np
+import copy
 import operator
 import TASK
 
 
 
-def set_date():
+def set_date_day():
+    # Set start and end depending on dependency
+    for task in TASK.my_task:
+        if task.dependency != '':
+            dependency = task.dependency
+            for predecessor in TASK.my_task:
+                if (predecessor.name == dependency) & (predecessor.type[5:10] == task.dependency_type):
+                    start = predecessor.end + predecessor.reserve
+            print(task.name)
+            task.start = start
+            task.end = task.start + task.duration
+
+def set_date_week():
     # Set start and end depending on dependency
     for task in TASK.my_task:
         if task.dependency != '':
@@ -15,12 +28,21 @@ def set_date():
             task.start = start
             task.end = task.start + task.duration
 
-def order_stem():
-    TASK.my_task.sort(key=operator.attrgetter('end'))
+def order_start():
+    task = copy.deepcopy(TASK.my_task)
+    task.sort(key=operator.attrgetter('start'))
+    return task
+
+def order_end():
+    task = copy.deepcopy(TASK.my_task)
+    task.sort(key=operator.attrgetter('end'))
+    return task
 
 def order_type():
+    task = copy.deepcopy(TASK.my_task)
+
     # Reverse order for Gantt chart
-    TASK.my_task.reverse()
+    task.reverse()
 
     # Group in terms of task
     result_all_0_intern = []
@@ -32,30 +54,31 @@ def order_type():
     result_jag_1_intern = []
     result_jag_1_extern = []
     result_jag_1_engineering = []
-    for task in TASK.my_task:
-        if task.type == 'eng (all_0)':
+    for my_task in result:
+        if my_task.type == 'eng (all_0)':
             result_all_0_engineering = result_all_0_engineering + [task]
-        if task.type == 'int (all_0)':
+        if my_task.type == 'int (all_0)':
             result_all_0_intern = result_all_0_intern + [task]
-        if task.type == 'ext (all_0)':
+        if my_task.type == 'ext (all_0)':
             result_all_0_extern = result_all_0_extern + [task]
-        if task.type == 'eng (jag_0)':
+        if my_task.type == 'eng (jag_0)':
             result_jag_0_engineering = result_jag_0_engineering + [task]
-        if task.type == 'int (jag_0)':
+        if my_task.type == 'int (jag_0)':
             result_jag_0_intern = result_jag_0_intern + [task]
-        if task.type == 'ext (jag_0)':
+        if my_task.type == 'ext (jag_0)':
             result_jag_0_extern = result_jag_0_extern + [task]
-        if task.type == 'eng (jag_1)':
+        if my_task.type == 'eng (jag_1)':
             result_jag_1_engineering = result_jag_1_engineering + [task]
-        if task.type == 'int (jag_1)':
+        if my_task.type == 'int (jag_1)':
             result_jag_1_intern = result_jag_1_intern + [task]
-        if task.type == 'ext (jag_1)':
+        if my_task.type == 'ext (jag_1)':
             result_jag_1_extern = result_jag_1_extern + [task]
-        if task.type == 'eng (vwb_0)':
+        if my_task.type == 'eng (vwb_0)':
             result_jag_0_engineering = result_jag_0_engineering + [task]
-        if task.type == 'int (vwb_0)':
+        if my_task.type == 'int (vwb_0)':
             result_jag_0_intern = result_jag_0_intern + [task]
-        if task.type == 'ext (vwb_0)':
+        if my_task.type == 'ext (vwb_0)':
             result_jag_0_extern = result_jag_0_extern + [task]
 
-    TASK.my_task = result_all_0_intern + result_all_0_extern + result_all_0_engineering + result_jag_0_intern + result_jag_0_extern + result_jag_0_engineering + result_jag_1_intern + result_jag_1_extern + result_jag_1_engineering
+    result = result_all_0_intern + result_all_0_extern + result_all_0_engineering + result_jag_0_intern + result_jag_0_extern + result_jag_0_engineering + result_jag_1_intern + result_jag_1_extern + result_jag_1_engineering
+    return result
--- RESULT.py
+++ RESULT.py
@@ -16,20 +16,7 @@ class result:
         self.path = 'car_management/timeplan'
         os.makedirs(self.path, exist_ok=True)
 
-        ORDER.set_date()
-
-        self.name = []
-        self.type = []
-        self.date_start = []
-        self.date_end = []
-
-
-
-        for task in TASK.my_task:
-            self.name = self.name + [task.name]
-            self.type = self.type + [task.type]
-            self.date_start = self.date_start + [task.start]
-            self.date_end = self.date_end + [task.end]
+        ORDER.set_date_day()
 
         self.color = {  'eng (all_0)': (0.2, 0.2, 0.2),
                         'int (all_0)': (0.6, 0.0, 0.0),
@@ -47,8 +34,8 @@ class result:
 
     def list(self):
         result = ''
-        for task in TASK.my_task:
-            result = result + str(task.start)[:10] + ' - ' + str(task.end)[:10] + ' (' + str(task.duration)[:2] + ' d): ' + task.type + ' - ' + task.name + '\n'
+        for my_task in TASK.my_task:
+            result = result + str(my_task.start)[:10] + ' - ' + str(my_task.end)[:10] + ' (' + str(my_task.duration)[:2] + ' d): ' + my_task.type + ' - ' + my_task.name + '\n'
 
         #file = open("list.txt", "w", exist_ok=True)
         file = open(self.path + "/list.txt", "w")
@@ -61,38 +48,49 @@ class result:
         file.close()
 
     def stem(self):
-        ORDER.order_stem()
+        task = ORDER.order_end()
+
+        name = []
+        type = []
+        date_start = []
+        date_end = []
+
+        for my_task in task:
+            name = name + [my_task.name]
+            type = type + [my_task.type]
+            date_start = date_start + [my_task.start]
+            date_end = date_end + [my_task.end]
 
         # Choose some nice level
-        level_max = 20
+        level_max = 30
         level_raw = np.linspace(1, level_max * 2 + 1, level_max + 1)
         level = []
         for i in level_raw:
             level = level + [i] + [-i]
-        level = np.tile(level, int(np.ceil(len(self.date_end) / 3)))[:len(self.date_end)]
+        level = np.tile(level, int(np.ceil(len(date_end) / 3)))[:len(date_end)]
 
         # Create figure and plot a stem plot with the date_end
         fig, ax = plt.subplots(figsize=(8.8, 4), constrained_layout=False)
-        ax.set(title="project car")
+        ax.set(title="project_car deadlines")
 
-        for i in range(len(self.date_end)):
-            markerline, stemline, baseline = ax.stem([self.date_end[i]], [level[i]], basefmt="k-", use_line_collection=True)
-            plt.setp(markerline, color=(self.color[self.type[i]]))
-            plt.setp(stemline, color=(self.color[self.type[i]]))
+        for i in range(len(date_end)):
+            markerline, stemline, baseline = ax.stem([date_end[i]], [level[i]], basefmt="k-", use_line_collection=True)
+            plt.setp(markerline, color=(self.color[type[i]]))
+            plt.setp(stemline, color=(self.color[type[i]]))
 
         # Add the y = 0 line in the middle
-        markerline, stemline, baseline = ax.stem([np.min(self.date_start), np.max(self.date_end)], [0, 0], basefmt="k-", use_line_collection=True)
+        markerline, stemline, baseline = ax.stem([np.min(date_start), np.max(date_end)], [0, 0], basefmt="k-", use_line_collection=True)
         plt.setp(markerline, mec="k", mfc="w", zorder=4)
         plt.setp(baseline, mec="k", mfc="w", zorder=3)
 
         # Label
         font = {#'family': 'normal',
                 'weight': 'normal',
-                'size': 5}
+                'size': 4}
         matplotlib.rc('font', **font)
 
         vert = np.array(['top', 'bottom'])[(level > 0).astype(int)]
-        for d, l, r, va in zip(self.date_end, level, self.name, vert):
+        for d, l, r, va in zip(date_end, level, name, vert):
             #ax.annotate(r, xy=(d, l), xytext=(-3, np.sign(l) * 3), textcoords="offset points", va=va, ha="right")
             ax.annotate(r, xy=(d, l), xytext=(-3, np.sign(l) * 3), textcoords="offset points", va='center', ha="right")
             ax.annotate(r, xy=(d, l), xytext=(-3, np.sign(l) * 3), textcoords="offset points", va='center', ha="right")
@@ -101,14 +99,15 @@ class result:
         #ax.get_xaxis().set_major_locator(mdates.MonthLocator(interval=4))  # 4 month interval
         ax.get_xaxis().set_major_formatter(mdates.DateFormatter("%d %b %Y"))
 
-        ax_min = np.min(self.date_start)
-        ax_max = np.max(self.date_end)
+        ax_min = np.min(date_start)
+        ax_max = np.max(date_end)
         ax_delta = ax_max - ax_min
-        ax_min = np.min(self.date_start) - 0.1 * ax_delta
-        ax_max = np.max(self.date_end) + 0.1 * ax_delta
+        ax_min = np.min(date_start) - 0.1 * ax_delta
+        ax_max = np.max(date_end) + 0.1 * ax_delta
 
         ax.set_xlim([ax_min, ax_max])
         plt.setp(ax.get_xticklabels(), rotation=30, ha="right")
+        plt.gcf().subplots_adjust(bottom=0.25)
 
         # Remove y axis and spines
         ax.get_yaxis().set_visible(False)
@@ -120,10 +119,12 @@ class result:
 
 
     def gantt(self):
-        ORDER.order_type()
+        #task = ORDER.order_start()
+        task = TASK.my_task
+
         df = []
-        for i in range(len(self.name)):
-            df = df + [dict(Task=self.name[i], Start=self.date_start[i], Finish=self.date_end[i], Resource=self.type[i])]
+        for i in range(len(task)):
+            df = df + [dict(Task=task[i].name, Start=task[i].start, Finish=task[i].end, Resource=task[i].type)]
 
         fig = ff.create_gantt(df, colors=self.color, index_col='Resource', show_colorbar=True)
         fig.show()
